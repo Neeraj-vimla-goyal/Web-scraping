09d25e3325be616137fdeadfbe24b4700d77058e
neeraj-vimla-goyal <neeraj.vimla.goyal@gmail.com>
2023-04-25 01:05:24 +0530

    added the email feature

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import smtplib
+import ssl
+import time
 import requests
 import selectorlib
-import time
-
+import os
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -23,10 +25,6 @@ def extract(source):
     return value
 
 
-def send_email():
-    print("Email Sent !")
-
-
 def store(_str):
     with open("data.txt", "a") as file:
         file.write(_str + "\n")
@@ -38,7 +36,35 @@ def read():
     return content
 
 
+def get_cred():
+    cred_dic = {}
+    for key in os.environ:
+        if key == "PASSWORD_SCRAPING_APP":
+            cred_dic["PASSWORD_SCRAPING_APP"] = os.environ["PASSWORD_SCRAPING_APP"]
+        elif key == "SENDER":
+            cred_dic["SENDER"] = os.environ["SENDER"]
+        elif key == "RECEIVER":
+            cred_dic["RECEIVER"] = os.environ["RECEIVER"]
+    return cred_dic
+
+
+def send_email(message, _username, _password, _receiver):
+    host = "smtp.gmail.com"
+    port = 465
+
+    _loc_username = _username
+    _loc_password = _password
+
+    _loc_receiver = _receiver
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(_loc_username, _loc_password)
+        server.sendmail(_loc_username, _loc_receiver, message)
+
+
 if __name__ == "__main__":
+
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
@@ -46,7 +72,16 @@ if __name__ == "__main__":
         file_content = read()
 
         if extracted not in file_content:
+
             if extracted != "No upcoming tours":
+                body = ""
                 store(extracted)
+                cred = get_cred()
+                user = cred["SENDER"]
+                passw = cred["PASSWORD_SCRAPING_APP"]
+                receiv = cred["RECEIVER"]
+                body = "Subject: New Tour Info-" + "\n" + "Hi " + receiv + ", " + "We have got the new tour info for you." + "\n"*2 + "Tour Name: " + extracted + "\n"*5 + "Thanks and Regards," + "\n" + user
+                body = body.encode("utf-8")
+                send_email(body, user, passw, receiv)
 
         time.sleep(1)
