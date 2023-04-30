73f5fa6b05885626566f937f79bc7cd761d0281f
neeraj-vimla-goyal <neeraj.vimla.goyal@gmail.com>
2023-04-30 21:15:36 +0530

    re organised the program as per opps concepts

--- main.py
+++ main.py
@@ -11,101 +11,112 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
-def scrape(url):
-    """
-    Scrape the page from the URL
-    """
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+class Event:
+    def scrape(self, url):
+        """
+        Scrape the page from the URL
+        """
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        """
+        Extract the event from the response
+        """
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class FileOps:
+    def store(self, _str):
+        """
+        Write the event into file:
+        """
+        with open("data.txt", "a") as file:
+            file.write(_str + "\n")
+
+    def read(self):
+        """
+        read the events from the file
+        """
+        with open("data.txt", "r") as file:
+            content = file.read()
+        return content
 
 
-def store(_str):
-    with open("data.txt", "a") as file:
-        file.write(_str + "\n")
+class Config:
+    def get_cred(self):
+        cred_dic = {}
+        for key in os.environ:
+            if key == "PASSWORD_SCRAPING_APP":
+                cred_dic["PASSWORD_SCRAPING_APP"] = os.environ["PASSWORD_SCRAPING_APP"]
+            elif key == "SENDER":
+                cred_dic["SENDER"] = os.environ["SENDER"]
+            elif key == "RECEIVER":
+                cred_dic["RECEIVER"] = os.environ["RECEIVER"]
+        return cred_dic
 
 
-def read():
-    with open("data.txt", "r") as file:
-        content = file.read()
-    return content
+class Email:
+    def send(self, message, _username, _password, _receiver):
+        host = "smtp.gmail.com"
+        port = 465
+
+        _loc_username = _username
+        _loc_password = _password
+
+        _loc_receiver = _receiver
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(_loc_username, _loc_password)
+            server.sendmail(_loc_username, _loc_receiver, message)
 
 
-def get_cred():
-    cred_dic = {}
-    for key in os.environ:
-        if key == "PASSWORD_SCRAPING_APP":
-            cred_dic["PASSWORD_SCRAPING_APP"] = os.environ["PASSWORD_SCRAPING_APP"]
-        elif key == "SENDER":
-            cred_dic["SENDER"] = os.environ["SENDER"]
-        elif key == "RECEIVER":
-            cred_dic["RECEIVER"] = os.environ["RECEIVER"]
-    return cred_dic
+class Database:
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
 
+    def read_db(self, _extracted):
+        row = _extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events where band=? AND city=? and date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
-def send_email(message, _username, _password, _receiver):
-    host = "smtp.gmail.com"
-    port = 465
-
-    _loc_username = _username
-    _loc_password = _password
-
-    _loc_receiver = _receiver
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(_loc_username, _loc_password)
-        server.sendmail(_loc_username, _loc_receiver, message)
-
-
-def get_connection(_db_name):
-    connection = sqlite3.connect(_db_name)
-    return connection
-
-
-def read_db(_extracted):
-    row = _extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    connection = get_connection("data.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events where band=? AND city=? and date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
-
-
-def write_db(_extracted):
-    row = _extracted.split(",")
-    row = [item.strip() for item in row]
-    connection = get_connection("data.db")
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
+    def write_db(self, _extracted):
+        row = _extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
 
 
 if __name__ == "__main__":
 
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
-        file_content = read()
+        file_operation = FileOps()
+        file_content = file_operation.read()
 
         if extracted != "No upcoming tours":
-            row = read_db(extracted)
+            dbops = Database(database_path="data.db")
+            row = dbops.read_db(extracted)
             if not row:
                 body = ""
                 # Writing into the file
-                store(extracted)
+                file_operation.store(extracted)
                 # Getting credentials for the emails
-                cred = get_cred()
+                config = Config()
+                cred = config.get_cred()
                 user = cred["SENDER"]
                 passw = cred["PASSWORD_SCRAPING_APP"]
                 receiv = cred["RECEIVER"]
@@ -113,9 +124,10 @@ if __name__ == "__main__":
                 body = "Subject: New Tour Info-" + "\n" + "Hi " + receiv + ", " + "We have got the new tour info for you." + "\n" * 2 + "Tour Name: " + extracted + "\n" * 5 + "Thanks and Regards," + "\n" + user
                 body = body.encode("utf-8")
                 # Sending the email body
-                send_email(body, user, passw, receiv)
+                email = Email()
+                email.send(body, user, passw, receiv)
                 # Writing into the database
-                write_db(extracted)
+                dbops.write_db(extracted)
 
                 time.sleep(1)
 
